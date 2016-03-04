0a84b1ecb61e3c05a7e264504b812b2af6f738f3
Andrey Balandin <vinc@fromru.com>
2016-03-04 14:27:42 +0500

    add 'include('qa.urls')' for '^question/' urls

--- ask/ask/views.py
+++ ask/ask/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def test(request, *args, **kwargs):
-    return HttpResponse('OK.\n' + str(request) + '\n' + str(args) + '\n' + str(kwargs))
+    return HttpResponse('OK. Test passed.')
